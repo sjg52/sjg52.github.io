028e2f22b5dd142b62a1c7afe22715146208b367
kumoko <453513858@qq.com>
2025-05-20 03:56:47 +0800

    Site updated: 2025-05-20 03:56:47

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1747684475050";const preCache = ["/images/taichi.png","/images/tiantai.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1747684594806";const preCache = ["/images/taichi.png","/images/tiantai.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
